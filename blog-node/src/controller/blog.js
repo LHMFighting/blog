634e8ce6dd357646b7bf9a04b14b03c142af1cbb
吕惠敏 <lvhuimin@lvhuimindeMacBook-Pro.local>
2019-06-05 23:33:52 +0800

    处理postdata

--- blog-node/src/controller/blog.js
+++ blog-node/src/controller/blog.js
@@ -17,6 +17,28 @@ const getList = (author, keyword) => {
     ]
 }
 
+const getDetail = (id) => {
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1546610491112,
+            author: 'zhangsan'
+        }
+    ]
+}
+
+const newBlog = (blogData = {}) => {
+    // blogData是一个对象，包含title content属性
+    console.log('newBlog blogData', blogData)
+    return {
+        id: 3,  // 表示新建博客，插入到数据表里面的id
+    }
+}
+
 module.exports = {
-    getList
+    getList,
+    getDetail,
+    newBlog
 }
