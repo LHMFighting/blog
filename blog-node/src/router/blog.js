dcd777cbfe323faebe354af5b9805a36a034df6b
lvhm1 <lvhm1@meicloud.com>
2019-06-14 15:09:59 +0800

    增加管理员界面

--- blog-node/src/router/blog.js
+++ blog-node/src/router/blog.js
@@ -19,10 +19,22 @@ const handleBlogRouter = (req, res) => {
 
     // 获取博客列表
     if (method === 'GET' && req.path === '/api/blog/list') {
-        const author = req.query.author || ''
+        let author = req.query.author || ''
         const keyword = req.query.keyword || ''
         // const listData = getList(author, keyword)
         // return new SuccessModel(listData)
+
+        if (req.query.isadmin) {
+            // 管理员界面
+            const loginCheckResult = loginCheck(req)
+            if (loginCheckResult) {
+                // 未登录
+                return loginCheckResult
+            }
+            // 强制查询自己的博客
+            author = req.session.username
+        }
+
         const result = getList(author, keyword)
         return result.then(listData => {
             return new SuccessModel(listData)
@@ -47,7 +59,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
         
         req.body.author = req.session.username
@@ -62,7 +74,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
         const result = updateBlog(id, req.body)
         return result.then(val => {
@@ -79,7 +91,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
         const author = req.session.username
         const result = delBlog(id, author)
